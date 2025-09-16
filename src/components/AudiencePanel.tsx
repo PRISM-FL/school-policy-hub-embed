adfcee0412a5a4854b1da753ff2e858e4ba2cc7a
Damian Thomas <dimitrisonic@gmail.com>
2025-09-16 19:38:19 -0400

    add recommended resource blocks

--- src/components/AudiencePanel.tsx
+++ src/components/AudiencePanel.tsx
@@ -21,37 +21,41 @@ export function AudiencePanel({
   return (
     <div {...props} className="bg-accent-color p-2">
       <div
-        className="p-2"
+        className="p-4"
         style={{ backgroundImage: `url(${ruledBg})`, backgroundSize: "300px" }}
       >
         <div>
           <div className="mb-1">
             <img src={image} alt="dummy image" />
           </div>
-          <h1 className="text-2xl font-bold pb-1 mb-2 border-b-2 border-black">
+          <h1 className="text-3xl font-bold pb-1 mb-2 border-b-2 border-black">
             {title}
           </h1>
           <p className="font-bold text-sm">{description}</p>
         </div>
         <div>
-          <h2>Recommended Resources</h2>
-          {!articles && (
-            <p>
-              Could not load recommended resources. Please refresh the page and
-              try again.
-            </p>
-          )}
-          {articles &&
-            articles.map(({ title, excerpt, url }) => (
-              <article key={url}>
-                <a className="w-full h-full" href={url}>
-                  <header>
-                    <h3>{title}</h3>
-                  </header>
-                  <p>{excerpt}</p>
-                </a>
-              </article>
-            ))}
+          <h2 className="font-bold text-2xl pb-1 mb-2 border-b-2 border-black">
+            Recommended Resources
+          </h2>
+          <div className="flex flex-col gap-4">
+            {!articles && (
+              <p>
+                Could not load recommended resources. Please refresh the page
+                and try again.
+              </p>
+            )}
+            {articles &&
+              articles.map(({ title, excerpt, url }) => (
+                <article key={url} className="flex flex-col text-white">
+                  <a className="w-full h-full bg-fg-color p-2" href={url}>
+                    <header className="border-b-2 border-white pb-1 mb-1">
+                      <h3 className="text-xl font-bold">{title}</h3>
+                    </header>
+                    <p>{excerpt}</p>
+                  </a>
+                </article>
+              ))}
+          </div>{" "}
         </div>
       </div>
     </div>
