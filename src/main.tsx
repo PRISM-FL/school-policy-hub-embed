81d7a31a64ae3dae0a381459049ad5a1b93e4c69
Damian Thomas <dimitrisonic@gmail.com>
2025-09-16 17:28:48 -0400

    remove unused imports

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./global.css";
 import App from "./App.tsx";
-import { getFeaturedArticles, getRecentArticles } from "./lib/api.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
