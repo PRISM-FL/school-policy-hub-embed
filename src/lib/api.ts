afcf11bb78b554e05623a3b94b536d7aeb49bd3a
Damian Thomas <dimitrisonic@gmail.com>
2025-10-16 20:01:34 -0400

    remove console.log from api

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -44,7 +44,6 @@ export async function getFeaturedArticles(
       article.featured,
   );
 
-  console.log(audienceArticles);
   return audienceArticles
     .map((article) => ({
       title: article.title ?? "Not Found",
